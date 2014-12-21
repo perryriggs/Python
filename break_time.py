f9b70e436af3c3b323c725b6fb91b31dbb9a1a56
perryriggs <perry.riggs@gmail.com>
2014-12-20 22:21:08 -0500

    loop 3 times
    Added a while loop so it is call three times.

--- break_time.py
+++ break_time.py
@@ -1,5 +1,9 @@
 import time
 import webbrowser
 
-time.sleep(10)
-webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+x = 1
+while x <= 3:
+    time.sleep(10)
+    webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    x = x+1
+

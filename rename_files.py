bd2f6fe1443854756d60b2c7b91319eaae2c6b34
perryriggs <perry.riggs@gmail.com>
2014-12-21 10:55:15 -0500

    rename complete
    Final code to rename files by removing all numbers from the name.

--- rename_files.py
+++ rename_files.py
@@ -1,1 +1,15 @@
 #Lesson 1 Use functions.  rename_files.py
+import os
+#define a function to rename a file
+def rename_files():
+        file_list = os.listdir(r"H:\config\Desktop\GitHub\Python\prank")
+        saved_path = os.getcwd()
+        os.chdir(r"H:\config\Desktop\GitHub\Python\prank")
+        for file_name in file_list:
+                print "renaming "+file_name+"\tto\t"+file_name.translate(None,"0123456789")
+                os.rename(file_name,file_name.translate(None,"0123456789"))
+        os.chdir(saved_path)
+# and here is where that function is called.
+print "Operating System is ["+os.name+"]"
+rename_files()
+print("files renamed")
